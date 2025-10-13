ec299ee298bb6d48378fe11854f9829b5241a29d
Your Name <you@example.com>
2025-10-13 12:45:31 +0700

    Update Feature

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
+            'root' => '/home/metb4532/public_html/portal.metincaprimaworks.com/uploads',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
